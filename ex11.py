c58a08cf3ffba100fcf135f191c29e52d71a6996
deadbolts <joeblow@gmail.com>
2018-03-02 15:40:36 -0800

    add ex11, ex12, and ex13

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+#adding a character inside the single quotes prints
+#the character inside the prompt answer.
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+#taking out the space within the single quotes just adds a space for answer prompt
